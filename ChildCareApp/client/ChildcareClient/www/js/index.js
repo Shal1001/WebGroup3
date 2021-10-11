fe6bba79e34b5b6101ccb2f8f5cdd622213f18e5
Saajidh <5965869+Saajidh@users.noreply.github.com>
2021-10-11 18:12:56 +1100

    AndroidPersistentFileLocation

--- ChildCareApp/client/ChildcareClient/www/js/index.js
+++ ChildCareApp/client/ChildcareClient/www/js/index.js
@@ -1,4 +1,4 @@
-var serverIP = "192.168.0.100";
+var serverIP = "192.168.0.100"; // This is the local computer IP
 
 //This method created the button from the approriate children
 async function createButton(textBtn, childid) {
@@ -11,9 +11,9 @@ async function createButton(textBtn, childid) {
   );
 }
 
-// This variable hold the username of the  
+// This variable hold the username of the eduactor. ****take from login*****
 var educatorUsername = "saajidh.nizam@cqumail.com";
-// this method fetch the details of the childrens an educator 
+// this method fetch the details of the childrens an educator
 function fetchChildrenDetails() {
   var childByEduURL = `http://${serverIP}:3000/children/${educatorUsername}`;
   $.ajax({
@@ -24,10 +24,14 @@ function fetchChildrenDetails() {
       if (data.length != 0) {
         console.log(data);
         storeChildrenDetails(data);
-        
+
         var childrendDetailsArray = retrieveChildrensFromLocalStore();
-        
-        for (var childernNo = 0; childernNo < childrendDetailsArray.length; childernNo++) {
+
+        for (
+          var childernNo = 0;
+          childernNo < childrendDetailsArray.length;
+          childernNo++
+        ) {
           var childName = childrendDetailsArray[childernNo].Name;
           var childid = childrendDetailsArray[childernNo]._id;
 
@@ -43,47 +47,39 @@ function fetchChildrenDetails() {
 var valData;
 
 function fetchChildrenDetailsById(ChildId) {
-  var childByIdURL = `http://${serverIP}:3000/children/id/${ChildId}`;
+  var childrenStorageData = localStorage.getItem(
+    "childrenofEducatorLocalStorage"
+  );
+  var childrenStorageDataArray = JSON.parse(childrenStorageData)[0];
 
-  $.ajax({
-    type: "GET",
-    url: childByIdURL,
-    dataType: "JSON",
-    success: function (data) {
-      if (data.length != 0) {
-        var childName = data[0].Name;
-        var childid = data[0]._id;
-        var childRoom = data[0].Room;
-        console.log(data);
-
-        $(".reflection-name").empty();
-        $(".reflection-name").append(childName);
-        $(".reflection-room").empty();
-        $(".reflection-room").append(childRoom);
-      } else {
-        $("#btnContainer").append("<p>No children assign to you room</p>");
-      }
-    },
-  });
+  for (
+    var childernNo = 0;
+    childernNo < childrenStorageDataArray.length;
+    childernNo++
+  ) {
+    var childName = childrenStorageDataArray[childernNo].Name;
+    var childid = childrenStorageDataArray[childernNo]._id;
+    var childRoom = childrenStorageDataArray[childernNo].Room;
+    if (childid == ChildId) {
+      $(".reflection-name").empty();
+      $(".reflection-name").append(childName);
+      $(".reflection-room").empty();
+      $(".reflection-room").append(childRoom);
+    }
+  }
 }
 
-// this method store the details of the children from the database in the localStroge 
+// this method store the details of the children from the database in the localStroge
 function storeChildrenDetails(childrenEndroledforEducator) {
   try {
     if (localStorage) {
       var childrenofEducatorLocalStorage;
       if (!localStorage["childrenofEducatorLocalStorage"])
         childrenofEducatorLocalStorage = [];
-      else
-      localStorage.clear();
-        /*childrenofEducatorLocalStorage = JSON.parse(
-          localStorage["childrenofEducatorLocalStorage"]
-        );*/
+      else localStorage.clear();
       if (!(childrenofEducatorLocalStorage instanceof Array))
         childrenofEducatorLocalStorage = [];
       childrenofEducatorLocalStorage.push(childrenEndroledforEducator);
-
-      
 
       localStorage.setItem(
         "childrenofEducatorLocalStorage",
@@ -101,13 +97,14 @@ function storeChildrenDetails(childrenEndroledforEducator) {
   }
 }
 
-function retrieveChildrensFromLocalStore(){
-
-  try{
-    var childrenStorageData = localStorage.getItem("childrenofEducatorLocalStorage");
+// This Method retrives the Children Details from the localstorage
+function retrieveChildrensFromLocalStore() {
+  try {
+    var childrenStorageData = localStorage.getItem(
+      "childrenofEducatorLocalStorage"
+    );
     var childrenStorageDataArray = JSON.parse(childrenStorageData)[0];
     return childrenStorageDataArray;
-
   } catch (error) {
     console.error(
       "Error detected while the store data to the local storage" + error
@@ -116,9 +113,7 @@ function retrieveChildrensFromLocalStore(){
       "Unable to retrive details of the children, Please try again in moment. Sorry for the inconvininace"
     );
   }
-
 }
-
 
 $(document).ready(function () {
   fetchChildrenDetails();
@@ -126,42 +121,73 @@ $(document).ready(function () {
     $("body").pagecontainer("change", "#daily-reflections-page", {
       transition: "slide",
     });
-
-    var valueText = $(this).html();
+    //var valueText = $(this).html();
     var childId = $(this).attr("data-index-number");
     fetchChildrenDetailsById(childId);
   });
 
-  $("#newtest").on("click", function () {
-    $("#test").text(valData.toString());
-  });
+  //This method work with ce
+  let app = {
+    init: function () {
+      document
+        .getElementById("camera-take-images")
+        .addEventListener("click", app.takephoto);
+    },
+    takephoto: function () {
+      let option = {
+        quality: 80,
+        destinationType: Camera.DestinationType.DATA_URL, // 1
+        sourceType: Camera.PictureSourceType.CAMERA,
+        mediaType: Camera.MediaType.PICTURE, // use the image gallery
+        encodingType: Camera.EncodingType.JPEG,
+        cameraDirection: Camera.Direction.BACK, //use the back camera
+        targetWidth: 1024,
+      };
+      navigator.camera.getPicture(app.ftw, app.wtf, option);
+    },
+    // This fuction is exicuted when the camera is successful
+    ftw: function (imageURI) {
+      console.log(imageURI);
+
+      var base64Img = imageURI;
+      var image = new Image();
+      image.src = "data:image/jpeg;base64," + base64Img;
+
+      $("#child-images").append(
+        `<img display:none;width:60px;height:60px; src='${image}'></img>`
+      );
+      var childimgURL = `http://${serverIP}:3000/img`;
+
+      var form = new FormData();
+      form.append("image", image);
+
+      //form.append("image", fileInput.files[0], `/${imageURI}`);
+
+      console.log(form);
+      var settings = {
+        url: "http://192.168.0.100:3000/img",
+        method: "POST",
+        timeout: 0,
+        headers: {
+          test: "test",
+          "Content-Type": "application/json",
+        },
+        data: JSON.stringify({
+          fileName: `${new Date().getTime()}.jpg`,
+          fileContent: base64Img,
+        }),
+      };
+
+      $.ajax(settings).done(function (response) {
+        console.log(response);
+      });
+
+      console.log(imageURI);
+    },
+    wtf: function (message) {
+      alert(message);
+    },
+  };
+
+  document.addEventListener("deviceready", app.init);
 });
-
-let app = {
-  init: function () {
-    document
-      .getElementById("child-images")
-      .addEventListener("click", app.takephoto);
-  },
-  takephoto: function () {
-    let option = {
-      quality: 80,
-      destinationType: Camera.DestinationType.FILE_URI, // 1
-      sourceType: Camera.PictureSourceType.CAMERA,
-      mediaType: Camera.MediaType.PICTURE, // use the image gallery
-      encodingType: Camera.EncodingType.JPEG,
-      cameraDirection: Camera.Direction.BACK, //use the back camera
-      targetWidth: 1024,
-    };
-    navigator.camera.getPicture(app.ftw, app.wtf, option);
-  },
-  // This fuction is exicuted when the camera is successful
-  ftw: function (imageURI) {
-    $("#child-images").append(`<img src='${imageURI}'></img>`);
-  },
-  wtf: function (message) {
-    alert(message);
-  },
-};
-
-document.addEventListener("deviceready", app.init);
--- ChildCareApp/server/apiServer.js
+++ ChildCareApp/server/apiServer.js
@@ -1,15 +1,26 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const port = 3000
-app.use(express.json())    
+const express = require('express');
+const fs = require("fs");
+const cors = require('cors');
+const jimp = require("jimp"); // This helps to save the images in a small file size
+const app = express();
+const port = 3000;
+app.use(express.json({ limit: '25mb' }));
+app.use(express.urlencoded({ limit: '25mb' }));;
 app.use(cors());
 
 const baseURL = '192.168.0.100';
 
+const folderName = './images'
 
-const {MongoClient, ObjectId} = require('mongodb');
-//let ObjectId = MongoClient.ObjectId;
+try {
+	if (!fs.existsSync(folderName)) {
+		fs.mkdirSync(folderName);
+	}
+} catch (err) {
+	console.error(err);
+}
+
+const { MongoClient, ObjectId } = require('mongodb');
 
 const uri = "mongodb+srv://admin:BvJSJXzrrkzcnAwX@childcarecluster.mj85c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -20,44 +31,44 @@ let collectionChildren;
 
 async function retriveChildren(eduFilter) {
 	try {
-	  // create an instance of MongoClient
-	  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-	  await client.connect(); // client.connect() returens a promise so await hold the operations from further exicuting
-	  const childCollectionCurser = await client
-		.db("ChildCareDatabase")
-		.collection("Child")
-		.find({
-			"Educator.Username": {$eq: eduFilter}
-		})
-		.toArray();
+		// create an instance of MongoClient
+		const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+		await client.connect(); // client.connect() returens a promise so await hold the operations from further exicuting
+		const childCollectionCurser = await client
+			.db("ChildCareDatabase")
+			.collection("Child")
+			.find({
+				"Educator.Username": { $eq: eduFilter }
+			})
+			.toArray();
 		collectionChildren = childCollectionCurser;
 		console.log(childCollectionCurser);
 		client.close();
 	} catch (e) {
-	  console.error("Error detected:" + e);
+		console.error("Error detected:" + e);
 	}
-  }
+}
 
-  var childObj;
-  async function retriveChildrenId(childFilter) {
-	  console.log(childFilter);
+var childObj;
+async function retriveChildrenId(childFilter) {
+	console.log(childFilter);
 	try {
-	  // create an instance of MongoClient
-	  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-	  await client.connect(); // client.connect() returens a promise so await hold the operations from further exicuting
-	  const childCollectionCurser = await client
-		.db("ChildCareDatabase")
-		.collection("Child")
-		.find({
-			"_id": ObjectId(childFilter)	
-		}).toArray();
+		// create an instance of MongoClient
+		const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+		await client.connect(); // client.connect() returens a promise so await hold the operations from further exicuting
+		const childCollectionCurser = await client
+			.db("ChildCareDatabase")
+			.collection("Child")
+			.find({
+				"_id": ObjectId(childFilter)
+			}).toArray();
 		childObj = childCollectionCurser;
 		console.log(childCollectionCurser);
 		client.close();
 	} catch (e) {
-	  console.error("Error detected:" + e);
+		console.error("Error detected:" + e);
 	}
-  }
+}
 
 
 app.get('/children/:educator', cors(), (req, res) => {
@@ -74,7 +85,26 @@ app.get('/children/id/:id', cors(), (req, res) => {
 	res.send(childObj)
 })
 
+const uploadImage = async (req, res, next) => {
 
-app.listen(port, baseURL , () => {
-  console.log(`Childcare app listening at http://localhost:${port}`)
+}
+
+app.post("/img", cors(), (req, res) => {
+
+	// Base64 string
+	const data = req.body.fileContent;
+	const fileName = `./images/${req.body.fileName}`;
+	// Convert base64 to buffer 
+	const buffer = Buffer.from(data, "base64");
+	const i = jimp.read(buffer, (err, res) => {
+		if (err) throw new Error(err);
+		res.quality(80).write(fileName);
+	  });
+	console.log(i);
+	console.log("res.body");
+	res.send("Uploaded");
+})
+
+app.listen(port, baseURL, () => {
+	console.log(`Childcare app listening at http://localhost:${port}`)
 })
